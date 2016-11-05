6dfbfa3ea8e667a0495e9afc3e4207cfed9a0867
Thutmose <elpatricimo@gmail.com>
2016-11-04 21:09:13 -0400

    add code for essentials

---
+++ src/main/java/thut/essentials/areacontrol/AreaManager.java
@@ -0,0 +1,16 @@
+package thut.essentials.areacontrol;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+public class AreaManager
+{
+    List<Area> areas = Lists.newArrayList();
+
+    public AreaManager()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+}
