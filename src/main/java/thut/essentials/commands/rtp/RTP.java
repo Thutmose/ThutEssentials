e00b1cde1f0a553e98f0633cf8b4eb075cb415ba
Thutmose <elpatricimo@gmail.com>
2016-11-14 11:50:04 -0400

    fix RTP command.

--- src/main/java/thut/essentials/commands/rtp/RTP.java
+++ src/main/java/thut/essentials/commands/rtp/RTP.java
@@ -1,74 +1,100 @@
 package thut.essentials.commands.rtp;
 
+import java.util.Random;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
-import thut.essentials.commands.misc.Spawn.PlayerMover;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import thut.essentials.ThutEssentials;
+import thut.essentials.commands.misc.Spawn.PlayerMover;
 import thut.essentials.util.BaseCommand;
 import thut.essentials.util.ConfigManager;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.block.BlockLiquid;
-import net.minecraft.block.BlockAir;
 
-import java.util.Random;
+public class RTP extends BaseCommand
+{
 
-public class RTP extends BaseCommand {
+    public RTP()
+    {
+        super("rtp", 0);
+    }
 
-	public RTP() {
-		super("rtp", 0);
-	}
+    /** Return whether the specified command parameter index is a username
+     * parameter. */
+    @Override
+    public boolean isUsernameIndex(String[] args, int index)
+    {
+        return index == 0;
+    }
 
-	@Override
-	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		 EntityPlayer player = getCommandSenderAsPlayer(sender);
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+    {
+        EntityPlayer player;
+        if (args.length == 1)
+        {
+            if (sender instanceof EntityPlayer)
+                if (!ThutEssentials.perms.hasPermission((EntityPlayer) sender, "rtp.other"))
+                    throw new CommandException("You do not have permission to RTP someone else.");
+            player = getPlayer(server, sender, args[0]);
+        }
+        else
+        {
+            player = getCommandSenderAsPlayer(sender);
+        }
+        BlockPos position;
+        int n = 100;
+        while ((position = checkSpot(player)) == null && n-- > 0)
+            ;
+        if (position != null) PlayerMover.setMove(player, player.dimension, position, null);
+        else sender.addChatMessage(new TextComponentString("No spot found."));
+    }
 
-		 BlockPos position;
-		 while ((position = checkSpot(player)) == null);
-		 
-		 PlayerMover.setMove(player, player.dimension, position, null);
-	}
+    private boolean isValid(IBlockState ground, IBlockState lower, IBlockState upper)
+    {
+        return ground.isBlockNormalCube() && validMaterial(lower.getMaterial()) && validMaterial(upper.getMaterial());
+    }
 
-	// Calculate the next position to check.
-	private BlockPos calculatePos()
-	{
-		Random rand = new Random();
-		int x = rand.nextInt(ConfigManager.INSTANCE.rtpdistance);
-		int z = rand.nextInt(ConfigManager.INSTANCE.rtpdistance);
-		int y = 64;
-		return new BlockPos(x, y, z);
-	}
-	
-	private BlockPos checkSpot(EntityPlayer player)
-	{
-		BlockPos position = calculatePos();
-		IBlockState standing = player.getEntityWorld().getBlockState(position);
-		IBlockState mid = player.getEntityWorld().getBlockState(position);
-		IBlockState breath = player.getEntityWorld().getBlockState(position);
-		
-		// If the block we're standing on is air or liquid or if the two blocks we take up are liquid then return null, check failed.
-		if (standing.getBlock() instanceof BlockLiquid || standing.getBlock() instanceof BlockAir || mid.getBlock() instanceof BlockLiquid || standing.getBlock() instanceof BlockLiquid)
-		{
-			return null;
-		}
-		while (!(mid.getBlock() instanceof BlockAir || breath.getBlock() instanceof BlockAir))
-		{
-			// Make a new position of y+1
-			position = new BlockPos(position.getX(), position.getY()+1, position.getZ());
-			standing = player.getEntityWorld().getBlockState(position);
-			mid = player.getEntityWorld().getBlockState(position);
-			breath = player.getEntityWorld().getBlockState(position);
-			// If the block we're standing on is air or liquid or if the two blocks we take up are liquid then return null, check failed.
-			if (standing.getBlock() instanceof BlockLiquid || standing.getBlock() instanceof BlockAir || mid.getBlock() instanceof BlockLiquid || standing.getBlock() instanceof BlockLiquid)
-			{
-				return null;
-			}
-			// If we reached to top.... for some reason..........
-			if (position.getY() > 256) return null;
-		}
-		return position;
-		
-	}
-	
+    private boolean validMaterial(Material mat)
+    {
+        return !mat.isSolid() && !mat.isLiquid();
+    }
+
+    // Calculate the next position to check.
+    private BlockPos calculatePos()
+    {
+        Random rand = new Random();
+        int x = (int) (Math.signum(rand.nextGaussian()) * rand.nextInt(ConfigManager.INSTANCE.rtpdistance));
+        int z = (int) (Math.signum(rand.nextGaussian()) * rand.nextInt(ConfigManager.INSTANCE.rtpdistance));
+        int y = 252;
+        return new BlockPos(x, y, z);
+    }
+
+    private BlockPos checkSpot(EntityPlayer player)
+    {
+        BlockPos position = calculatePos();
+        IBlockState ground = player.getEntityWorld().getBlockState(position);
+        IBlockState lower = player.getEntityWorld().getBlockState(position.up());
+        IBlockState upper = player.getEntityWorld().getBlockState(position.up(2));
+        // If the block we're standing on is air or liquid or if the two blocks
+        // we take up are liquid then return null, check failed.
+        if (isValid(ground, lower, upper)) { return position; }
+        if (ground.getMaterial().isLiquid()) return null;
+        while (!isValid(ground, lower, upper) && position.getY() > 0)
+        {
+            // Make a new position of y+1
+            position = position.down();
+            ground = player.getEntityWorld().getBlockState(position);
+            lower = player.getEntityWorld().getBlockState(position.up());
+            upper = player.getEntityWorld().getBlockState(position.up(2));
+            if (ground.getMaterial().isLiquid()) return null;
+        }
+        return position.up(2);
+
+    }
+
 }
