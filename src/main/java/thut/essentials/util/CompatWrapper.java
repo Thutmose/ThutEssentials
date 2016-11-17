ff2894fc7cf65c5c03bfbce177c3777edcaee228
Thutmose <elpatricimo@gmail.com>
2016-11-16 20:57:43 -0400

    fix breaking compat wrapper.

--- src/main/java/thut/essentials/util/CompatWrapper.java
+++ src/main/java/thut/essentials/util/CompatWrapper.java
@@ -12,7 +12,6 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityList.EntityEggInfo;
-import net.minecraft.entity.MoverType;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -28,36 +27,36 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CompatWrapper
 {
-    public static final ItemStack nullStack = ItemStack.field_190927_a;
+    public static final ItemStack nullStack = null;
 
     public static ItemStack fromTag(NBTTagCompound tag)
     {
-        return new ItemStack(tag);
+        return ItemStack.loadItemStackFromNBT(tag);
     }
 
     public static ItemStack copy(ItemStack in)
     {
-        return in.copy();
+        return ItemStack.copyItemStack(in);
     }
 
     public static Entity createEntity(World world, String in)
     {
-        return EntityList.createEntityByIDFromName(new ResourceLocation(in), world);
+        return EntityList.createEntityByIDFromName(in, world);
     }
 
     public static Entity createEntity(World world, ResourceLocation in)
     {
-        return EntityList.createEntityByIDFromName(in, world);
+        return EntityList.createEntityByIDFromName(in.toString(), world);
     }
 
     public static Entity createEntity(World world, Entity in)
     {
-        return EntityList.createEntityByIDFromName(EntityList.func_191301_a(in), world);
+        return EntityList.createEntityByIDFromName(EntityList.getEntityString(in), world);
     }
 
     public static void moveEntitySelf(Entity in, double x, double y, double z)
     {
-        in.moveEntity(MoverType.SELF, x, y, z);
+        in.moveEntity(x, y, z);
     }
 
     public static void sendChatMessage(ICommandSender to, ITextComponent message)
@@ -80,13 +79,14 @@ public class CompatWrapper
     public static ItemStack setStackSize(ItemStack stack, int amount)
     {
         if (amount <= 0) { return nullStack; }
-        stack.func_190920_e(amount);
+        stack.stackSize = amount;
         return stack;
     }
 
     public static int getStackSize(ItemStack stack)
     {
-        return stack.func_190916_E();
+        if (stack == nullStack || stack.stackSize < 0 || stack.getItem() == null) { return 0; }
+        return stack.stackSize;
     }
 
     public static boolean isValid(ItemStack stack)
@@ -102,13 +102,13 @@ public class CompatWrapper
 
     public static void setAnimationToGo(ItemStack stack, int num)
     {
-        stack.func_190915_d(num);
+        stack.animationsToGo = num;
     }
 
     public static int increment(ItemStack in, int amt)
     {
-        in.func_190917_f(amt);
-        return in.func_190916_E();
+        in.stackSize += amt;
+        return in.stackSize;
     }
 
     public static List<ItemStack> makeList(int size)
@@ -121,39 +121,28 @@ public class CompatWrapper
 
     public static void rightClickWith(ItemStack stack, EntityPlayer player, EnumHand hand)
     {
-        ItemStack old = player.getHeldItem(hand);
-        player.setHeldItem(hand, stack);
-        stack.getItem().onItemRightClick(player.worldObj, player, hand);
-        player.setHeldItem(hand, old);
+        stack.getItem().onItemRightClick(stack, player.worldObj, player, hand);
     }
 
     public static NBTTagCompound getTag(ItemStack stack, String name, boolean create)
     {
-        NBTTagCompound ret = stack.getSubCompound(name);
-        if (ret == null)
-        {
-            if (!stack.hasTagCompound()) stack.setTagCompound(new NBTTagCompound());
-            ret = new NBTTagCompound();
-            stack.getTagCompound().setTag(name, ret);
-        }
-        return ret;
+        return stack.getSubCompound(name, create);
     }
 
     public static void processInitialInteract(Entity in, EntityPlayer player, EnumHand hand, ItemStack stack)
     {
-        in.processInitialInteract(player, hand);
+        in.processInitialInteract(player, stack, hand);
     }
 
     public static boolean interactWithBlock(Block block, World worldIn, BlockPos pos, IBlockState state,
             EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY,
             float hitZ)
     {
-        return block.onBlockActivated(worldIn, pos, state, playerIn, hand, side, hitX, hitY, hitZ);
+        return block.onBlockActivated(worldIn, pos, state, playerIn, hand, heldItem, side, hitX, hitY, hitZ);
     }
 
     public static EntityEggInfo getEggInfo(String name, int colour1, int colour2)
     {
-        return new EntityEggInfo(new ResourceLocation(name), colour1, colour2);
+        return new EntityEggInfo(name, colour1, colour2);
     }
-
 }
