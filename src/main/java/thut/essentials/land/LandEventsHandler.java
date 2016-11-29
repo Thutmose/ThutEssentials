8632e2fb55d1adb75216345af768a9011afdbc8b
Thutmose <elpatricimo@gmail.com>
2016-11-28 23:50:00 -0400

    tweaks to teleporting, hopefully fixes some of the jitter on tp.
    Also attempt to fix the leaving land message on login.

--- src/main/java/thut/essentials/land/LandEventsHandler.java
+++ src/main/java/thut/essentials/land/LandEventsHandler.java
@@ -91,6 +91,7 @@ public class LandEventsHandler
     public void PlayerLoggin(PlayerLoggedInEvent evt)
     {
         EntityPlayer entityPlayer = evt.player;
+        lastLeaveMessage.put(entityPlayer.getUniqueID(), System.currentTimeMillis() + 5);
         LandManager.getTeam(entityPlayer);
     }
 
