d76caa779c023532d52384b02216b39b4b6c32f3
Thutmose <elpatricimo@gmail.com>
2018-08-10 19:31:32 -0400

    Adds /listteams which does part of what is in issue #40

---
+++ src/main/java/thut/essentials/commands/land/ListTeams.java
@@ -0,0 +1,40 @@
+package thut.essentials.commands.land;
+
+import java.util.Map;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import thut.essentials.land.LandManager;
+import thut.essentials.land.LandManager.LandTeam;
+import thut.essentials.util.BaseCommand;
+
+public class ListTeams extends BaseCommand
+{
+
+    public ListTeams()
+    {
+        super("listteams", 0);
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+    {
+        sendTeamList(sender);
+    }
+
+    public void sendTeamList(ICommandSender sender)
+    {
+        Map<String, LandTeam> teamMap = LandManager.getInstance()._teamMap;
+        sender.sendMessage(new TextComponentString(TextFormatting.AQUA + "Team List:"));
+        for (String s : teamMap.keySet())
+        {
+            String emptyTip = "";
+            if (teamMap.get(s).member.size() == 0) emptyTip = "(EMPTY)";
+            sender.sendMessage(new TextComponentString(
+                    TextFormatting.AQUA + "[" + TextFormatting.YELLOW + s + TextFormatting.AQUA + "] " + emptyTip));
+        }
+    }
+}
