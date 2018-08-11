c284a16c3f73b52e97f8a7ae1d79867055c2730d
Thutmose <elpatricimo@gmail.com>
2018-08-10 22:24:45 -0400

    some more cleanup of stuff

--- src/main/java/thut/essentials/commands/land/Join.java
+++ src/main/java/thut/essentials/commands/land/Join.java
@@ -5,9 +5,7 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.server.permission.IPermissionHandler;
 import net.minecraftforge.server.permission.PermissionAPI;
-import net.minecraftforge.server.permission.context.PlayerContext;
 import thut.essentials.land.LandEventsHandler;
 import thut.essentials.land.LandManager;
 import thut.essentials.land.LandManager.LandTeam;
@@ -26,19 +24,15 @@ public class Join extends BaseCommand
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
     {
         EntityPlayer player = getPlayerBySender(sender);
-        IPermissionHandler manager = PermissionAPI.getPermissionHandler();
-        PlayerContext context = new PlayerContext(player);
         String teamname = args[0];
         LandTeam teamtojoin = LandManager.getInstance().getTeam(teamname, false);
         LandTeam oldTeam = LandManager.getTeam(player);
         if (oldTeam == teamtojoin) throw new CommandException("You are already in that team!");
         if (teamtojoin != null)
         {
-            boolean canJoinInvite = manager.hasPermission(player.getGameProfile(),
-                    LandEventsHandler.PERMJOINTEAMINVITED, context);
+            boolean canJoinInvite = PermissionAPI.hasPermission(player, LandEventsHandler.PERMJOINTEAMINVITED);
             canJoinInvite = canJoinInvite && LandManager.getInstance().hasInvite(player.getUniqueID(), teamname);
-            boolean canJoinNoInvite = manager.hasPermission(player.getGameProfile(),
-                    LandEventsHandler.PERMJOINTEAMNOINVITE, context);
+            boolean canJoinNoInvite = PermissionAPI.hasPermission(player, LandEventsHandler.PERMJOINTEAMNOINVITE);
             canJoinInvite = canJoinInvite
                     || teamtojoin.teamName.equalsIgnoreCase(ConfigManager.INSTANCE.defaultTeamName);
             if (canJoinInvite && teamtojoin.member.size() == 0)
