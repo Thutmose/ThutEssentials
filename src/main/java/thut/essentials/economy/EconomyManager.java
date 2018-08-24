9c0dba29aa22742db4bd40ab8de64e3ae788e4ad
Thutmose <elpatricimo@gmail.com>
2018-08-24 00:31:14 -0400

    fixes a message in economy

--- src/main/java/thut/essentials/economy/EconomyManager.java
+++ src/main/java/thut/essentials/economy/EconomyManager.java
@@ -213,7 +213,7 @@ public class EconomyManager
                     }
                     if (count < number)
                     {
-                        player.sendMessage(new TextComponentString(TextFormatting.RED + "Insufficient Items"));
+                        player.sendMessage(new TextComponentString(TextFormatting.RED + "You have Insufficient Items"));
                         return false;
                     }
                 }
