6e93ef9947c440d2f02cf221932b401b74493e52
Thutmose <elpatricimo@gmail.com>
2020-12-03 21:26:09 -0500

    partially fixes gen_structure

--- src/main/java/thut/essentials/commands/structures/Structuregen.java
+++ src/main/java/thut/essentials/commands/structures/Structuregen.java
@@ -2,7 +2,6 @@ package thut.essentials.commands.structures;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -16,18 +15,24 @@ import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.palette.UpgradeData;
+import net.minecraft.util.registry.DynamicRegistries;
+import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.feature.structure.StructureManager;
+import net.minecraft.world.gen.feature.structure.StructureStart;
 import net.minecraft.world.gen.feature.template.TemplateManager;
 import net.minecraft.world.server.ServerWorld;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.server.permission.DefaultPermissionLevel;
 import net.minecraftforge.server.permission.PermissionAPI;
 import thut.essentials.Essentials;
@@ -64,8 +69,8 @@ public class Structuregen
     private static SuggestionProvider<CommandSource> SUGGEST_NAMES = (ctx, sb) ->
     {
         final List<String> opts = Lists.newArrayList();
-        for (final Entry<RegistryKey<Structure<?>>, Structure<?>> loc : ForgeRegistries.STRUCTURE_FEATURES.getEntries())
-            opts.add(loc.getKey().getLocation().toString());
+        for (final ResourceLocation loc : WorldGenRegistries.CONFIGURED_STRUCTURE_FEATURE.keySet())
+            opts.add(loc.toString());
         return net.minecraft.command.ISuggestionProvider.suggest(opts, sb);
     };
 
@@ -73,149 +78,7 @@ public class Structuregen
             final List<Chunk> originals, final WorldGenRegionWrapper worldRegion, final int minY, final int maxY,
             final boolean doStructures)
     {
-        // final ServerWorld worldIn = worldRegion.world;
-        // final TemplateManager templateManager =
-        // worldIn.getStructureTemplateManager();
-        //
-        // final ChunkGenerator generator =
-        // worldIn.getChunkProvider().generator;
-        // final BiomeManager bman = worldIn.getBiomeManager();
-        //
-        // int s = 0;
-        // source.sendFeedback(new StringTextComponent("Initializing Chunks " +
-        // s++), false);
-        // for (final IChunk c : primers)
-        // generator.generateStructures(bman, c, generator, templateManager);
-        // source.sendFeedback(new StringTextComponent("Initializing Chunks " +
-        // s++), false);
-        // for (final IChunk c : primers)
-        // generator.generateStructureStarts(worldRegion, c);
-        // source.sendFeedback(new StringTextComponent("Initializing Chunks " +
-        // s++), false);
-        // for (final IChunk c : primers)
-        // generator.generateBiomes(c);
-        // source.sendFeedback(new StringTextComponent("Initializing Chunks " +
-        // s++), false);
-        // for (final IChunk c : primers)
-        // generator.makeBase(worldRegion, c);
-        // source.sendFeedback(new StringTextComponent("Initializing Chunks " +
-        // s++), false);
-        // for (final IChunk c : primers)
-        // generator.generateSurface(worldRegion, c);
-        // source.sendFeedback(new StringTextComponent("Initializing Chunks " +
-        // s++), false);
-        // for (final IChunk c : primers)
-        // generator.func_225550_a_(bman, c, Carving.AIR);
-        // source.sendFeedback(new StringTextComponent("Initializing Chunks " +
-        // s++), false);
-        // for (final IChunk c : primers)
-        // generator.func_225550_a_(bman, c, Carving.LIQUID);
-        // source.sendFeedback(new StringTextComponent("Initializing Chunks " +
-        // s++), false);
-        //
-        // final MutableBoundingBox box =
-        // MutableBoundingBox.getNewBoundingBox();
-        // box.minY = minY;
-        // box.maxY = maxY;
-        //
-        // final LazyOptional<IHasStructures> structs =
-        // worldIn.getCapability(WorldStructures.CAPABILITY);
-        //
-        // for (int i = 0; i < primers.size(); i++)
-        // {
-        // final ChunkPrimer c = (ChunkPrimer) primers.get(i);
-        // final Chunk o = originals.get(i);
-        // final ChunkPos p = o.getPos();
-        //
-        // box.minX = p.getXStart();
-        // box.minZ = p.getZStart();
-        // box.maxX = p.getXEnd();
-        // box.maxZ = p.getZEnd();
-        // structs.ifPresent(holder -> holder.removeStructures(null, box));
-        // for (int y = minY; y < maxY; y++)
-        // for (int x = box.minX; x <= box.maxX; x++)
-        // for (int z = box.minZ; z <= box.maxZ; z++)
-        // {
-        // final BlockPos pos = new BlockPos(x, y, z);
-        // final BlockState newstate = c.getBlockState(pos);
-        // worldIn.setBlockState(pos, newstate, 3 + 32);
-        // }
-        //
-        // for (final CompoundNBT compoundnbt : c.getEntities())
-        // EntityType.func_220335_a(compoundnbt, worldIn, (p_217325_1_) ->
-        // {
-        // o.addEntity(p_217325_1_);
-        // return p_217325_1_;
-        // });
-        //
-        // for (final TileEntity tileentity : c.getTileEntities().values())
-        // o.addTileEntity(tileentity);
-        //
-        // o.setStructureStarts(c.getStructureStarts());
-        // o.setStructureReferences(c.getStructureReferences());
-        //
-        // for (final Entry<Heightmap.Type, Heightmap> entry :
-        // c.getHeightmaps())
-        // if (ChunkStatus.FULL.getHeightMaps().contains(entry.getKey()))
-        // o.getHeightmap(entry.getKey())
-        // .setDataArray(entry.getValue().getDataArray());
-        //
-        // o.setLight(c.hasLight());
-        // o.markDirty();
-        // }
-        // source.sendFeedback(new StringTextComponent("Regenerating Features
-        // and Structures"), false);
-        // for (final IChunk c : primers)
-        // {
-        // final ChunkPos cpos = c.getPos();
-        // final int cx = cpos.x;
-        // final int cj = cpos.z;
-        // final int k = cx * 16;
-        // final int l = cj * 16;
-        // final BlockPos pos = new BlockPos(k, 0, l);
-        // final Biome biome = bman.getBiome(pos.add(8, 8, 8));
-        // final SharedSeedRandom random = new SharedSeedRandom();
-        // final long seed = random.setDecorationSeed(worldRegion.getSeed(), k,
-        // l);
-        // for (final GenerationStage.Decoration stage :
-        // GenerationStage.Decoration.values())
-        // try
-        // {
-        // int i = 0;
-        //
-        // for (final ConfiguredFeature<?, ?> feature :
-        // biome.getFeatures(stage))
-        // {
-        // if (!doStructures && feature.feature instanceof Structure<?>)
-        // {
-        // ++i;
-        // continue;
-        // }
-        // random.setFeatureSeed(seed, i, stage.ordinal());
-        // try
-        // {
-        // feature.place(worldRegion, generator, random, pos);
-        // }
-        // catch (final Exception exception)
-        // {
-        // final CrashReport crashreport =
-        // CrashReport.makeCrashReport(exception, "Feature placement");
-        // crashreport.makeCategory("Feature").addDetail("Id",
-        // ForgeRegistries.FEATURES.getKey(
-        // feature.feature)).addDetail("Description", () ->
-        // {
-        // return feature.feature.toString();
-        // });
-        // Essentials.LOGGER.error(crashreport.getCompleteReport());
-        // }
-        // ++i;
-        // }
-        // }
-        // catch (final Exception exception)
-        // {
-        // Essentials.LOGGER.error(exception);
-        // }
-        // }
+
     }
 
     private static int execute_reset(final CommandSource source, final int radius) throws CommandSyntaxException
@@ -251,77 +114,34 @@ public class Structuregen
         return 0;
     }
 
+    @SuppressWarnings("unchecked")
     private static int execute_generate(final CommandSource source, final String structname)
             throws CommandSyntaxException
     {
         final ServerPlayerEntity player = source.asPlayer();
 
+        final ResourceLocation key = new ResourceLocation(structname);
+        if (!WorldGenRegistries.CONFIGURED_STRUCTURE_FEATURE.keySet().contains(key)) return 1;
+
         final IChunk chunk = player.getEntityWorld().getChunkAt(player.getPosition());
         final ServerWorld worldIn = player.getServerWorld();
+        final ChunkGenerator generator = worldIn.getChunkProvider().generator;
+        final DynamicRegistries reg = worldIn.getServer().func_244267_aX();
         final TemplateManager templateManager = worldIn.getStructureTemplateManager();
-        final ResourceLocation key = new ResourceLocation(structname);
+        final int refs = 0;
+        final long seed = worldIn.getRandom().nextLong();
+        final Biome biome = worldIn.getBiome(player.getPosition());
+        final StructureManager structManager = worldIn.func_241112_a_();
+        final ChunkPos pos = chunk.getPos();
+        final StructureFeature<?, ?> feature = WorldGenRegistries.CONFIGURED_STRUCTURE_FEATURE.getOrDefault(key);
 
-        final Structure<?> structure = ForgeRegistries.STRUCTURE_FEATURES.getValue(key);
-
-        // final ChunkGenerator generator =
-        // worldIn.getChunkProvider().generator;
-        // ChunkPos chunkpos = chunk.getPos();
-        // final StructureStart<?> start =
-        // structure.getStartFactory().create(structure, chunkpos.x,
-        // chunkpos.z,
-        // MutableBoundingBox.getNewBoundingBox(), 0, generator.getSeed());
-        //
-        // final Biome biome = worldIn.getBiome(player.getPosition());
-        // final List<IChunk> primers = Lists.newArrayList();
-        // final List<Chunk> originals = Lists.newArrayList();
-        // start.init(generator, templateManager, chunkpos.x, chunkpos.z,
-        // biome);
-        // final SharedSeedRandom rand = new
-        // SharedSeedRandom(generator.getSeed());
-        // rand.setLargeFeatureSeed(generator.getSeed(), chunkpos.x,
-        // chunkpos.z);
-        // final MutableBoundingBox box = start.getBoundingBox();
-        //
-        // final int dx = box.maxX - box.minX;
-        // final int dz = box.maxZ - box.minZ;
-        // final int mx = dx / 2 + box.minX >> 4;
-        // final int mz = dz / 2 + box.minZ >> 4;
-        // final int ds = Math.max(dx, dz) >> 4;
-        // final List<IChunk> primers_for_starts = Lists.newArrayList();
-        // final Map<ChunkPos, IChunk> all_primers = Maps.newHashMap();
-        //
-        // chunkpos = new ChunkPos(mx, mz);
-        // final int pad = Math.max(10, ds);
-        // for (int x = mx - ds - pad; x <= mx + ds + pad; x++)
-        // for (int z = mz - ds - pad; z <= mz + ds + pad; z++)
-        // {
-        // final ChunkPos pos = new ChunkPos(x, z);
-        // final ChunkPrimer chunk2 = new ChunkPrimer(pos,
-        // UpgradeData.EMPTY);
-        // primers_for_starts.add(chunk2);
-        // all_primers.put(pos, chunk2);
-        // if (x < mx - ds || x > mx + ds) continue;
-        // if (z < mz - ds || z > mz + ds) continue;
-        // chunk = worldIn.getChunk(x, z);
-        // primers.add(chunk2);
-        // originals.add((Chunk) chunk);
-        // }
-        // final WorldGenRegionWrapper worldRegion = new
-        // WorldGenRegionWrapper(worldIn, primers_for_starts);
-        // Structuregen.regenerateChunks(source, primers, originals,
-        // worldRegion, box.minY, box.maxY + 1, true);
-        // source.sendFeedback(new StringTextComponent("Generating Requested
-        // Structure"), false);
-        // final LazyOptional<IHasStructures> structs =
-        // worldIn.getCapability(WorldStructures.CAPABILITY);
-        // structs.ifPresent(holder ->
-        // {
-        // for (final StructurePiece p : start.getComponents())
-        // holder.putStructure(key, p.getBoundingBox());
-        // });
-        // start.generateStructure(worldRegion, generator, rand, box,
-        // chunkpos);
-        // source.sendFeedback(new StringTextComponent("Done"), false);
+        final Structure<?> structure = feature.field_236268_b_;
+        @SuppressWarnings("rawtypes")
+        final StructureStart start = structure.createStructureStart(pos.x, pos.z, MutableBoundingBox
+                .getNewBoundingBox(), refs, seed);
+        start.func_230364_a_(reg, generator, templateManager, pos.x, pos.z, biome, feature.field_236269_c_);
+        start.func_230366_a_(worldIn, structManager, generator, worldIn.getRandom(), MutableBoundingBox
+                .func_236990_b_(), pos);
         return 0;
     }
 }
